31783d206d2a332c81909b67604091f46fc5f01a
Shicoreya Dobine <d.shicoreya@gmail.com>
2025-04-03 18:39:08 -0400

    Thursday

---
+++ Unit-2-Intro-To-C#/String Examples/String Examples/Program.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.Design;
+
+namespace String_Examples;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+    }
+    
+    string string1 = "a";
+    string string2 = "a";
+    
+    Console.WriteLine("String1 is " + string1)
+}
+
+if (string1.CompareTo(string2) < 0)
+{
+    Console.WriteLine("string 1 is less than string 2");
+}
+else
+{
+    Console.WriteLine("string 1 is not less than string 2");
+}
