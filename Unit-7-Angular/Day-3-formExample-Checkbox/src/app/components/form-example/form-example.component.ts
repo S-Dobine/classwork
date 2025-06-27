50fd1a06e04723b41ecd705acb7c2d4944a494e6
Shicoreya Dobine <d.shicoreya@gmail.com>
2025-06-26 21:55:32 -0400

    th

--- Unit-7-Angular/Day-3-formExample-Checkbox/src/app/components/form-example/form-example.component.ts
+++ Unit-7-Angular/Day-3-formExample-Checkbox/src/app/components/form-example/form-example.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 import { throwError } from 'rxjs';
 
 @Component({
   selector: 'form-example',  // Name used in html to reference the component <form-example>
   standalone: true,
-  imports: [FormsModule],  // Tell Angular we want Forms support for this code
+  imports: [FormsModule,CommonModule],  // Tell Angular we want Forms support for this code
   templateUrl: './form-example.component.html',
   styleUrl: './form-example.component.css'
 })
@@ -24,8 +25,16 @@ export class FormExampleComponent {
   // With each element in the array corresponding to a choice
   // Checkbox need to be included in the html a individual element
   // More tomorrow
-  //sports : any[] 
-  sports = ""
+  sports : any[] = [
+    {name: 'European Football', value: "Soccer"},
+    {name: 'American Football', value: "Football"},
+    {name: 'Baseball', value: "Baseball"},
+    {name: 'Cricket', value: "Cricket"},
+    {name: 'Hockey', value: "Hockey"},
+    {name: 'Other', value: "Other Sport"},
+    {name: 'None', value: "No Sports"}
+  ]
+  //sports = ""
  // Methods to handle interactions with the html/web page
   onSubmit() { // Handle the Submit button click on the  form
     this.buttonClicked = true
@@ -33,6 +42,16 @@ export class FormExampleComponent {
     console.log(`You entered ${this.firstName} in the First Name field`)
     console.log(`You entered ${this.lastName} in the Last Name field`)
     console.log(`You entered ${this.emailAddress} in the email field`)
-    console.log(`You entered ${this.sports} as sports you like`)
+    // console.log(`You entered ${this.sports} as sports you like`)
+    // console.table(this.sports)
+   // console.table(this.checkBoxResult)
+
+   this.checkBoxResult.forEach((aBox) => console.log(aBox.name)) 
   }
+
+  get checkBoxResult() { 
+    
+    return this.sports.filter((aCheckbox) => aCheckbox.checked)
+
+}
 }
